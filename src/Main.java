f329c1dd2554cd5e12f6e0589efbf5da9020194b
Kristy <krisstyle@inbox.ru>
2022-11-28 23:50:48 +0300

    Решение задачи 3 про работников

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        var boxer1 = 78.2;
-        var boxer2= 82.7;
-        System.out.println("Общий вес боксеров = " + (boxer1 + boxer2) + "кг");
-        var raznizaVesa = Math.abs(boxer1-boxer2);
-        System.out.println("Разница в весе составляет " + raznizaVesa + "кг");
-
-        raznizaVesa = boxer2-boxer1; //1способ
-        System.out.println("Разница в весе составляет " + raznizaVesa + "кг");
-        raznizaVesa = boxer2 % boxer1; //2способ
-        System.out.println("Разница в весе составляет " + raznizaVesa + "кг");
+        var hour = 640;
+        var job = 8;
+        var employee = hour / job;
+        System.out.println("Всего работников в компании – " + employee + " человек");
+            var bigEmployee = employee + 94;
+            var allEmployee = bigEmployee * job;
+            System.out.println("Если в компании работает " + bigEmployee + " человек, то всего " + allEmployee + " часов работы может быть поделено между сотрудниками");
     }
 }
